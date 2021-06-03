9704e9049d0caf805c8e4eda39c95c8820632f03
Ahmad Mustain Billah <mustain.bilah@gmail.com>
2021-06-03 20:55:50 +0700

    versi terbaru ngerjain bareng

--- js/index.js
+++ js/index.js
@@ -11,14 +11,60 @@ let stockHp = document.getElementById('stock');
 namaHp.innerHTML = hp.name;
 hargaHp.innerHTML = hp.price;
 stockHp.innerHTML = hp.stock;
+
+
+
+
 // console.log(object)
 let objCustomer = {}
 let btnOrder = document.getElementById('orderNow');
-btnOrder.addEventListener('click',() =>{
-    let namaCustomer = document.getElementById('nama');
-    let emailCustomer = document.getElementById('email');
-    let noHpCustomer = document.getElementById('noHp');
-    let jumlahDiBeli = document.getElementById('jumlah')
-    console.log('Helloooo');
-    console.log(namaCustomer.value,emailCustomer.value,noHpCustomer.value,jumlahDiBeli.value,'-----');
+
+// variable customer-details
+let namaCustDetails = document.getElementById('namaCust')
+let emailCustDetails = document.getElementById('emailCust')
+let noHpCustDetails = document.getElementById('noHpCust')
+let jumlahCustDetails = document.getElementById('jumlahItem')
+let totalCostDetails = document.getElementById('totalHarga')
+
+let divDetailsPurchased = document.getElementById('details-purchased')
+btnOrder.addEventListener('click',(event) =>{
+    event.preventDefault();
+    let nama = document.getElementById('nama').value;
+    let email = document.getElementById('email').value;
+    let noHp = document.getElementById('noHp').value;
+    let jumlahDiBeli = document.getElementById('jumlah').value;
+    const totalCost = jumlahDiBeli * hp.price;
+    console.log('ini total cost',totalCost)
+    // create object
+    objCustomer = {
+        nama,
+        email,
+        noHp,
+        jumlahDiBeli,
+        totalCost,
+    }
+
+    // ngurangin stock hp dan update html stock hp
+    hp.stock -= objCustomer.jumlahDiBeli
+    stockHp.innerHTML = hp.stock
+
+    // update div customer-details
+    namaCustDetails.innerHTML = objCustomer.nama
+    emailCustDetails.innerHTML = objCustomer.email
+    noHpCustDetails.innerHTML = objCustomer.noHp
+    jumlahCustDetails.innerHTML = objCustomer.jumlahDiBeli
+    totalCostDetails.innerHTML = objCustomer.totalCost
+    //show div detail purchased
+    divDetailsPurchased.style.display = 'flex'
 })
+
+// function onChange
+let elementJumlah = document.getElementById('jumlah')
+
+elementJumlah.addEventListener('mouseout', () => {
+    console.log(parseInt(elementJumlah.value,10), typeof parseInt(elementJumlah.value,10));
+    if(parseInt(elementJumlah.value,10) < 1){
+        alert('Jumlah yang ingin di order harus lebih dari 0')
+    }
+})
+
